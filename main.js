13f9d0ee55b97e726cf23adbe5059373e5edde81
GiPisano <pisanogiacomo9@gmail.com>
2024-01-24 17:39:28 +0100

    correction snack 1

--- main.js
+++ main.js
@@ -13,23 +13,71 @@ const vips = [
     'Maneskin',
 ];
 
-let posto = 1; 
-let objArray = [];
+
+
+
+
+const arrayObj = vips.map((vip, index) => {
+
 for(let vip of vips){
-   
+
     const guest = {
         tavolo: 'tavolo vip',
-        ospite: `${vip}`,
-        posto: `${posto++}`,
+        ospite: vip,
+        posto: index + 1,
     }
 
-    objArray.push(guest);
+  return guest;
 }
-
-console.log(objArray);
+})
+console.log(arrayObj);
 
 
 // Snack 2
 
+// const students = [
+//     {
+//         id: 213,
+//         name: 'Marco della Rovere',
+//         grades: 78,
+//     },
+//     {
+//         id: 110,
+//         name: 'Paola Cortellessa',
+//         grades: 96,
+//     },
+//     {
+//         id: 250,
+//         name: 'Andrea Mantegna',
+//         grades: 48,
+//     },
+//     {
+//         id: 145,
+//         name: 'Gaia Borromini',
+//         grades: 74,
+//     },
+//     {
+//         id: 196,
+//         name: 'Luigi Grimaldello',
+//         grades: 68,
+//     },
+//     {
+//         id: 102,
+//         name: 'Piero della Francesca',
+//         grades: 50,
+//     },
+//     {
+//         id: 120,
+//         name: 'Francesca da Polenta',
+//         grades: 84,
+//     }
+// ]
+
+
+// let nameStudents = [];
+
+// for(let student of students){
+//     console.log(student.name)
+// }
 
 
